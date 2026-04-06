e9e011e19fe0807a128377893cd2830ed9ce4083
nisrina salsa <nisrinahikmawan@gmail.com>
2026-04-06 10:40:01 +0700

    Initial commit

---
+++ src/main/java/com/pertemuan5/deploy5/Deploy5Application.java
@@ -0,0 +1,13 @@
+package com.pertemuan5.deploy5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deploy5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pertemuan5/deploy5/Deploy5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pertemuan5.deploy5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
